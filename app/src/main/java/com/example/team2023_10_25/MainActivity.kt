4c26e98139b57243a5766a297d02aa431dc1a10e
DwarfPorter <mrvlmor@gmail.com>
2023-10-25 21:17:55 +0300

    выскакивающее сообщение

--- app/src/main/java/com/example/team2023_10_25/MainActivity.kt
+++ app/src/main/java/com/example/team2023_10_25/MainActivity.kt
@@ -2,10 +2,13 @@ package com.example.team2023_10_25
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Toast.makeText(this, "Привет!", Toast.LENGTH_SHORT).show();
     }
 }
